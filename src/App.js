d82457384919a135240ea8aa60a36e5d7e412bee
Rishi Advani <rishi1999@gmail.com>
2018-10-07 17:03:49 -0400

    stable working version of single-user mode
    no known issues with single-user mode
    
    major changes:
    . added resizing ability
    . fixed spacing issues
    . fixed colors not updating when increasing size
    . added onBlur and onFocus functionality to SizeInput to improve UX
    . improved design of SizeInput

--- src/App.js
+++ src/App.js
@@ -1,28 +1,143 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var DEFAULT_SIZE = 10;
+var MAX_SIZE = 25;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.handleSizeChange = this.handleSizeChange.bind(this);
+		this.state = {size: DEFAULT_SIZE};
+	}
+
+	handleSizeChange(val) {
+		this.setState(
+			{size: val}
+			);
+	}
+
+	render() {
+		return (
+			<div className="App">
+			<center>
+			<SizeInput size={this.state.size} onSizeChange={this.handleSizeChange}/>
+			<br />
+			<hr />
+			<br />
+			<br />
+			<Grid size={this.state.size}/>
+			</center>
+			</div>
+			);
+	}
+}
+
+class SizeInput extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleFocus = this.handleFocus.bind(this);
+		this.handleBlur = this.handleBlur.bind(this);
+	}
+
+	handleChange(e) {
+		const val = e.target.value;
+		if (val == 0) {
+			this.props.onSizeChange("");
+		}
+		else if (!(isNaN(val))) {
+			this.props.onSizeChange(Math.min(val, MAX_SIZE));
+		}
+	}
+
+	handleFocus(e) {
+		this.props.onSizeChange("");
+	}
+
+	handleBlur(e) {
+		if (this.props.size == 0) {
+			this.props.onSizeChange(DEFAULT_SIZE);
+		}
+	}
+
+	render() {
+		return (
+			<input type="text" value={this.props.size} onChange={this.handleChange} onFocus={this.handleFocus} onBlur={this.handleBlur} />
+			);
+	}
+}
+
+class Grid extends Component {
+	render() {
+		return (
+			<table>
+			<tbody>
+			{this.createTable()}
+			</tbody>
+			</table>
+			);
+	}
+
+	createTable = () => {
+		let table = [];
+
+		for (let i = 0; i < this.props.size; i++) {
+			let children = [];
+
+			for (let j = 0; j < this.props.size; j++) {
+				console.log(this.props.size);
+				children.push(<Space key={i + "," + j + "," + this.props.size} iValue={i} jValue={j} size={this.props.size}/>);
+			}
+
+			table.push(<tr>{children}</tr>)
+		}
+
+		return table;
+	};
+}
+
+class Space extends Component {
+	constructor(props) {
+		super(props);
+		const bg = "rgb(" + Math.floor(this.props.iValue / this.props.size * 255) + ", " + Math.floor(this.props.jValue / this.props.size * 255) + ", 0)";
+		this.state = {
+			isOn: true,
+			bgColor: bg
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		if (this.state.isOn) {
+			const bg = "rgb(" + Math.floor(this.props.iValue / this.props.size * 255) + ", " + Math.floor(this.props.jValue / this.props.size * 255) + ", 0)";
+			this.setState(state => ({
+				isOn: false,
+				bgColor: bg
+			}));
+		} else {
+			const bg = "rgb(" + (255 - Math.floor(this.props.iValue / this.props.size * 255)) + ", " + (255 - Math.floor(this.props.jValue / this.props.size * 255)) + ", 255)";
+			this.setState(state => ({
+				isOn: true,
+				bgColor: bg
+			}));
+		}
+	}
+
+	render() {
+		const bStyle = {
+			backgroundColor: this.state.bgColor,
+			width: 80/this.props.size + "vw",
+			height: 70/this.props.size + "vh",
+			border: 0,
+			padding: 0,
+			margin: "-0.7vh" // to remove vertical button margins
+		};
+		return (
+			<td style={{padding: "0"}}><button style={bStyle} onMouseOver={this.handleClick}></button></td>
+			);
+	}
 }
 
 export default App;
